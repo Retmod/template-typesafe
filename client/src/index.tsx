fc517839d0a8fd3e7b0f47511081a0f7e37dfd37
RedCrafter07 <gh@r07.dev>
2022-09-24 17:23:14 +0200

    Use app

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
-import Wrappers from './wrappers';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement,
 );
-root.render(<Wrappers />);
+root.render(<App />);
