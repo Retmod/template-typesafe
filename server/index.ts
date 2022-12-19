2462330b60f8a506c2477f438603244883d21fc9
RedCrafter07 <gh@r07.dev>
2022-12-19 20:09:51 +0100

    Prisma, API routes & more

--- server/index.ts
+++ server/index.ts
@@ -1,34 +1,39 @@
 import express from 'express';
 import path from 'path';
 import chalk from 'chalk';
-import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
+import { fileURLToPath } from 'url';
+import * as trpc from '@trpc/server';
+import { z } from 'zod';
+
+const prisma = new PrismaClient();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config();
+
+const t = trpc.initTRPC.create();
+
+const appRouter = t.router({
+	hello: t.procedure
+		.input(z.string())
+		.output(z.string())
+		.query((inp) => {
+			return `Hello ${inp}`;
+		}),
+	getUsers: t.procedure.query(() => {
+		return prisma.user.findMany();
+	}),
+});
+
+export type AppRouter = typeof appRouter;
+
+await prisma.$connect();
+console.log(chalk.blue('[DATABASE]: Connected!'));
 
 const app = express();
-
-const MongoDBConfig: {
-	ip: string;
-	port: string;
-	db: string;
-	username: string;
-	password: string;
-} = {
-	db: 'main',
-	ip: '0.0.0.0',
-	port: '27017',
-	password: 'passwd',
-	username: 'user',
-};
-
-mongoose
-	.connect(
-		`mongodb://${MongoDBConfig.username}:${MongoDBConfig.password}@${MongoDBConfig.ip}:${MongoDBConfig.port}`,
-		{
-			dbName: MongoDBConfig.db,
-		},
-	)
-	.then(() => {
-		console.log(chalk.blue('[DATABASE]: Connected!'));
-	});
 
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
@@ -40,4 +45,9 @@ app.get('/', (req, res) => {
 
 app.listen(3000, () => {
 	console.log(chalk.green(`[SERVER]: Listening on port 3000`));
+});
+
+process.on('exit', () => {
+	console.log(chalk.red('[SERVER]: Shutting down...'));
+	prisma.$disconnect();
 });
