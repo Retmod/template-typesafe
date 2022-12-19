244dfe39d7d4875920b8700100491ee7ff51911e
RedCrafter07 <gh@r07.dev>
2022-12-19 20:42:28 +0100

    Fetch greeting

--- client/src/routes/home.tsx
+++ client/src/routes/home.tsx
@@ -1,8 +1,11 @@
+import { trpc } from '../util/trpc';
+
 export default function Home() {
+	const hi = trpc.hello.useQuery('User');
 	return (
 		<div className='bg-zinc-800 text-white min-h-screen p-2'>
 			<div className='mx-auto container'>
-				<p>Hi.</p>
+				<p>{hi.data || 'Loading...'}</p>
 			</div>
 		</div>
 	);
