5f448ff77c6f5ef96bcde3d64dae4ca75048de59
RedCrafter07 <gh@r07.dev>
2022-12-19 20:42:40 +0100

    Add tRPC wrappers

--- client/src/wrappers.tsx
+++ client/src/wrappers.tsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { httpBatchLink } from '@trpc/react-query';
 import Router from './router';
+import { trpc } from './util/trpc';
 
 export default function Wrappers() {
+	const [queryClient] = useState(new QueryClient());
+	const [trpcClient] = useState(() =>
+		trpc.createClient({
+			links: [httpBatchLink({ url: '/trpc' })],
+		}),
+	);
+
 	return (
-		<>
-			{/* Insert your wrappers here. They should go around the <Router /> class. */}
-			<Router />
-		</>
+		<trpc.Provider client={trpcClient} queryClient={queryClient}>
+			<QueryClientProvider client={queryClient}>
+				<Router />
+			</QueryClientProvider>
+		</trpc.Provider>
 	);
 }
